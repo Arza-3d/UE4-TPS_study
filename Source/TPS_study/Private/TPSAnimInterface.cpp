b3cec8ca696f988cf1409d4dc3dfe4fabbb60d02
Forest Guhareza <arza.3d@gmail.com>
2019-08-10 19:37:05 +0700

    probabbly gonna delete this later

---
+++ Source/TPS_study/Private/TPSAnimInterface.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSAnimInterface.h"
+
+/*
+// Add default functionality here for any ITPSAnimInterface functions that are not pure virtual.
+int ITPSAnimInterface::WeaponIndexIsSetTo(int weaponIndex)
+{
+	return weaponIndex;
+}*/
